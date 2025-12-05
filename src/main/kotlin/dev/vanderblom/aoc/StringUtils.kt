41f1be3114d4370df4390f1b8ea442488a2df137
Martijn van der Blom <martijn@teamofteams.nl>
2025-12-05 19:40:39 +0100

    Day2

--- src/main/kotlin/dev/vanderblom/aoc/StringUtils.kt
+++ src/main/kotlin/dev/vanderblom/aoc/StringUtils.kt
@@ -2,5 +2,9 @@ package dev.vanderblom.aoc
 
 fun String.splitInHalf(): Pair<String, String> {
     require(length.isEven()) { "length must be even" }
-    return this.substring(0, length/2) to this.substring(length/2)
+    return this.substring(0, length / 2) to this.substring(length / 2)
 }
+
+fun String.toRange(delimiter: String = "-") = this.split(delimiter)
+    .map(String::toLong)
+    .let { (from, to) -> (from..to) }
--- src/test/kotlin/dev/vanderblom/aoc/y2025/Day02.kt
+++ src/test/kotlin/dev/vanderblom/aoc/y2025/Day02.kt
@@ -4,6 +4,7 @@ import dev.vanderblom.aoc.AbstractDay
 import dev.vanderblom.aoc.isEqual
 import dev.vanderblom.aoc.isEven
 import dev.vanderblom.aoc.splitInHalf
+import dev.vanderblom.aoc.toRange
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
@@ -21,14 +22,14 @@ class Day02 : AbstractDay() {
     @Order(2)
     fun `part one - actual`() {
         assertThat(partOne(actualInput))
-            .isEqualTo(1L)
+            .isEqualTo(28846518423L)
     }
 
     @Test
     @Order(3)
     fun `part two - example`() {
         assertThat(partTwo(exampleInput))
-            .isEqualTo(1)
+            .isEqualTo(4174379265)
     }
 
     @Test
@@ -38,22 +39,64 @@ class Day02 : AbstractDay() {
             .isEqualTo(1)
     }
 
-    private fun partOne(input: List<String>): Long {
-        return parseInput(input)
-            .map { it.toString() }
-            .filter { it.length.isEven() }
-            .filter { it.splitInHalf().isEqual() }
-            .sumOf { it.toLong() }
+    private fun partOne(input: List<String>) = parseInput(input[0])
+        .map { it.toString() }
+        .filter { it.length.isEven() }
+        .filter { it.splitInHalf().isEqual() }
+        .sumOf { it.toLong() }
+
+
+    private fun partTwo(input: List<String>) = parseInput(input[0])
+        .map { it.toString() }
+        .filter { it.isInvalidId() }
+        .sumOf { it.toLong() }
+
+    private fun parseInput(input: String) = input
+        .split(",")
+        .map { it.toRange("-") }
+        .flatMap { it.toList() }
+
+    @Test
+    fun `1 is invalid`() {
+        assertThat("1".isInvalidId()).isFalse
     }
 
-    private fun parseInput(input: List<String>): List<Long> {
-        return input[0]
-            .split(",")
-            .map { it.split("-").map(String::toLong) }
-            .flatMap { (from, to)  -> (from..to).toList()}
+    @Test
+    fun `12341234 is invalid`() {
+        assertThat("12341234".isInvalidId()).isTrue
     }
 
-    private fun partTwo(input: List<String>): Int {
-        return input.size
+    @Test
+    fun `123123123 is invalid`() {
+        assertThat("123123123".isInvalidId()).isTrue
+    }
+
+    @Test
+    fun `1212121212 is invalid`() {
+        assertThat("1212121212".isInvalidId()).isTrue
+    }
+
+    @Test
+    fun `1111111 is invalid`() {
+        assertThat("1111111".isInvalidId()).isTrue
+    }
+
+    fun String.isInvalidId(): Boolean {
+        if (length > 1 && this.groupBy { it }.size == 1) {
+            println(this)
+            return true
+        }
+
+        repeat(length / 2) { i ->
+            if (chunked(i + 1)
+                    .groupBy { it }
+                    .size == 1
+            ) {
+                println(this)
+                return true
+            }
+        }
+
+        return false
     }
 }
